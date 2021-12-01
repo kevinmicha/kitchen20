8467c9e04b19f74bd2ed452fb0333d5c90151cd2
Mateo Bentura <mateo.bentura-larregui@imt-atlantique.net>
2021-12-01 21:15:20 +0100

    Copy files with format and add to csv. Output to test csv.

--- add-audios.py
+++ add-audios.py
@@ -8,66 +8,81 @@ elif len(sys.argv) == 1:
     input_folder = "custom-audios"
     output_folder = "script-test"
 else:
-    sys.exit(str(len(sys.argv))+" arguments present. Please enter an input folder followed by an output folder.")
+    sys.exit("Given "+str(len(sys.argv))+" arguments. Please, ONLY enter an input folder followed by an output folder.")
 
 print("=============================================")
 print("Running script to add audios to the database.")
 print("=============================================")
 
 print("Obtaining audios from folder '"+input_folder+"'")
-print("Outputing audios to folder '"+output_folder+"'")
+print("Outputting audios to folder '"+output_folder+"'")
 
 input_dir = os.path.join(os.getcwd(), input_folder)
 output_dir = os.path.join(os.getcwd(), output_folder)
 
-# Define target categories: modify according to the dataset
+try:
+    print("Creating output folder...")
+    os.mkdir(output_folder)
+except:
+    print("Output folder exists.")
 
+# Define target categories: modify according to the dataset
 with open('categories.txt') as f:
     categories = [category for category in f.read().splitlines()]
 
 print("Dataset containing "+str(len(categories))+" categories.")
 category_codes = {category:str(i) for i, category in enumerate(categories)}
 
-usr_id = "LL"
+# Define the user who recorded the examples
+usr_id = "LL" # IDEA add date to differentiate new recordings of the same category
+# IDEA Prompt user in script to directly add id
 
-df_k20 = pd.read_csv('kitchen20.csv').drop(columns=['Unnamed: 0','Unnamed: 0.1'])
-print(df_k20.head())
+df = pd.read_csv('kitchen20.csv').drop(columns=['Unnamed: 0','Unnamed: 0.1'])
 
-# TODO check, with user_id if files are not already added
+if any(df['usr_id'] == usr_id):
+    print("There exists recordings by user '"+usr_id+"' in the database.")
+    ans = input("Do you wish to continue? [yes/no] ")
+    if ans != 'yes' and ans != '' :
+        sys.exit("Exiting.")
+    else:
+        print("Continuing script.")
 
-audio_categories = sorted(os.listdir(input_dir), key=lambda item: int(category_codes[item]))
+
+categories = sorted(os.listdir(input_dir), key=lambda item: int(category_codes[item]))
 
 folds = [10]
-id = 0 # TODO begin id according to number of items in the csv
+id = len(df)
 
-cols = ['category', 'fold', 'orig_idx', 'path', 'take', 'target', 'usr_id']
-df = pd.DataFrame(columns=cols)
+cols = list(df.keys())
 
-# TODO iterate over all categories
-audio_category = audio_categories[0]
-category_dir = os.path.join(input_dir, audio_category)
-print("Accessing '"+audio_category+"', with label",category_codes[audio_category])
-p = re.compile('\d+')
-for audio in sorted(os.listdir(category_dir)):
-    audio_path = os.path.join(category_dir,audio)
-    take = chr(int(p.findall(audio)[0])+64)
-    fold = random.choice(folds)
-    audio_name = str(fold-1)+'-'+usr_id+'-'+take+'-'+category_codes[audio_category]+'.wav'
-    # print(audio_name)
-    # output_path = os.path.join(output_dir, audio_name)
-    # shutil.copyfile(audio_path, output_path)
-    line = pd.DataFrame([[audio_category,fold,id,os.path.join(output_folder,audio_name),take,category_codes[audio_category],usr_id]], columns=cols)
-    df = df.append(line, ignore_index=True)
-    id += 1
-print(df)
+for category in categories:
+    category_dir = os.path.join(input_dir, category)
+    print("Accessing '"+category+"', label=",category_codes[category])
+    p = re.compile('\d+')
+    for audio in sorted(os.listdir(category_dir)):
+        audio_path = os.path.join(category_dir,audio)
+        take = chr(int(p.findall(audio)[0])+64)
+        fold = random.choice(folds)
+        attributes = [fold-1,usr_id,take,category_codes[category]]
+        audio_name = "".join([str(attr)+'-' for attr in attributes])[:-1]+'.wav'
+        path = os.path.join(output_folder,audio_name)
+        if any(df['path'] == path):
+            print("There already exists a file with the same name.")
+        else:
+            copy_path = os.path.join(output_dir, audio_name)
+            shutil.copyfile(audio_path, copy_path)
 
+        line = pd.DataFrame([[  category,
+                                fold,
+                                id,
+                                path,
+                                take,
+                                category_codes[category],
+                                usr_id]],
+                                columns=cols)
+        df = df.append(line, ignore_index=True)
+        id += 1
 
 df.to_csv('test.csv')
-# for audio_category in audio_categories:
-#     category_dir = input_dir+audio_category
-#     print("Accessing '"+audio_category+"' audios")
-#     # audio_list =
 
-
-# for root, audio_category, audio_files in os.walk(input_dir):
-#     print("Accessing '"+audio_files+"' audios")
+print("Audios added succesfully to database.")
